0e1ef6cb221e9d354811edfe508518e72caf5e36
abdusman2014 <abdusman2014@gmail.com>
2023-08-03 19:16:07 +0500

    clear activeRoomId when user moves back from chat screen

--- lib/views/chat_message_screen.dart
+++ lib/views/chat_message_screen.dart
@@ -41,105 +41,104 @@ class _ChatMessageScreenState extends State<ChatMessageScreen> {
       });
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(roomName),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      backgroundColor: ThemeColors.chatScreenBackgroundColor,
-      body: SafeArea(
-        child: Column(children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-                stream: chatProvider.getMessagesByGroupId(roomId),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasData) {
-                    List<QueryDocumentSnapshot<Object?>>? messages =
-                        snapshot.data?.docs.reversed.toList();
-                    // if (messages!.isNotEmpty) {
-                    //   ChatMessage chat = ChatMessage.fromMap(
-                    //       messages[0].data() as Map<String, dynamic>);
-                    //   if (chat.roomId != roomId) {
-                    //     NotificationService.showNotification(
-                    //         from: roomName, message: chat.message);
-                    //   } else {
-                    //     print("donot show notification");
-                    //   }
-                    // }
-                    return Container(
-                      //  color: Colors.white,
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(52),
-                            bottomRight: Radius.circular(52),
-                          )),
-                      child: ListView.builder(
-                          reverse: true,
-                          controller: _scrollController,
-                          padding: const EdgeInsets.all(10),
-                          itemCount: snapshot.data?.docs.length,
-                          // reverse: true,
-                          // controller: scrollController,
-                          itemBuilder: (context, index) {
-                            ChatMessage chatMessage = ChatMessage.fromMap(
-                                messages![index].data()
-                                    as Map<String, dynamic>);
-                            bool isMe = (chatMessage.sentBy == up.appUser!.uid);
-                            return ChatComponent(
-                              message: chatMessage.message,
-                              isMe: isMe,
-                              time: chatMessage.sentAt,
-                            );
-                          }),
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                }),
-          ),
-          Container(
-            height: 55,
-            width: MediaQuery.of(context).size.width * 5 / 6,
-            padding: const EdgeInsets.all(16),
-            margin: const EdgeInsets.only(top: 12, bottom: 12),
-            decoration: const BoxDecoration(
-                color: ThemeColors.chatScreenTextEditColor,
-                borderRadius: BorderRadius.all(Radius.circular(40))),
-            // color: Colors.red,
-            child: Row(
-              children: [
-                Expanded(
-                    child: TextField(
-                  controller: _textController,
-                  onChanged: (value) {
-                    setState(() {
-                      textMessage = value;
-                    });
-                  },
-                  style: const TextStyle(color: Colors.white),
-                )),
-                GestureDetector(
-                    onTap: () {
-                      if (textMessage == "") {
-                        return;
-                      }
-                      onSendMessage();
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 8),
-                      child: Icon(
-                        Icons.send_rounded,
-                        size: 27,
-                        color: (textMessage != "") ? Colors.white : Colors.grey,
-                      ),
-                    ))
-              ],
+    return WillPopScope(
+      onWillPop: () async {
+        print("back");
+        chatProvider.activeRoomId = "";
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(roomName),
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        backgroundColor: ThemeColors.chatScreenBackgroundColor,
+        body: SafeArea(
+          child: Column(children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                  stream: chatProvider.getMessagesByGroupId(roomId),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      List<QueryDocumentSnapshot<Object?>>? messages =
+                          snapshot.data?.docs.reversed.toList();
+                      return Container(
+                        //  color: Colors.white,
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(52),
+                              bottomRight: Radius.circular(52),
+                            )),
+                        child: ListView.builder(
+                            reverse: true,
+                            controller: _scrollController,
+                            padding: const EdgeInsets.all(10),
+                            itemCount: snapshot.data?.docs.length,
+                            // reverse: true,
+                            // controller: scrollController,
+                            itemBuilder: (context, index) {
+                              ChatMessage chatMessage = ChatMessage.fromMap(
+                                  messages![index].data()
+                                      as Map<String, dynamic>);
+                              bool isMe =
+                                  (chatMessage.sentBy == up.appUser!.uid);
+                              return ChatComponent(
+                                message: chatMessage.message,
+                                isMe: isMe,
+                                time: chatMessage.sentAt,
+                              );
+                            }),
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  }),
             ),
-          ),
-        ]),
+            Container(
+              height: 55,
+              width: MediaQuery.of(context).size.width * 5 / 6,
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.only(top: 12, bottom: 12),
+              decoration: const BoxDecoration(
+                  color: ThemeColors.chatScreenTextEditColor,
+                  borderRadius: BorderRadius.all(Radius.circular(40))),
+              // color: Colors.red,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _textController,
+                    onChanged: (value) {
+                      setState(() {
+                        textMessage = value;
+                      });
+                    },
+                    style: const TextStyle(color: Colors.white),
+                  )),
+                  GestureDetector(
+                      onTap: () {
+                        if (textMessage == "") {
+                          return;
+                        }
+                        onSendMessage();
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 8),
+                        child: Icon(
+                          Icons.send_rounded,
+                          size: 27,
+                          color:
+                              (textMessage != "") ? Colors.white : Colors.grey,
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
