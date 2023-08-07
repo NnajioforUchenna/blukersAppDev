bee3276a0a3b4fcf8b09747f9edb12f562987c53
terra-labor <terralabor.dev@gmail.com>
2023-08-07 11:17:20 -0500

    industry img-url, temporarily hide total number

--- lib/views/common_views/industry_head_panel.dart
+++ lib/views/common_views/industry_head_panel.dart
@@ -1,5 +1,6 @@
 import 'package:bulkers/providers/user_provider.dart';
 import 'package:bulkers/services/on_hover.dart';
+import 'package:bulkers/services/responsive.dart';
 import 'package:bulkers/utils/styles/index.dart';
 import 'package:bulkers/views/common_views/applicant_count.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,11 @@ class IndustryHeadPanel extends StatelessWidget {
     return OnHover(
       child: Row(
         children: [
+          Image.network(
+            industry.imageUrl!,
+            width: Responsive.isDesktop(context) ? 70 : 50,
+          ),
+          const SizedBox(width: 20),
           Text(
             industry.name,
             style: const TextStyle(
@@ -25,13 +31,13 @@ class IndustryHeadPanel extends StatelessWidget {
               color: ThemeColors.grey1ThemeColor,
             ),
           ),
-          Spacer(),
-          ApplicantCount(
-            count: up.userRole == 'company'
-                ? industry.getApplicantCount()
-                : industry.getNumberOfJobPosts(),
-            color: ThemeColors.grey1ThemeColor,
-          ),
+          // const Spacer(),
+          // ApplicantCount(
+          //   count: up.userRole == 'company'
+          //       ? industry.getApplicantCount()
+          //       : industry.getNumberOfJobPosts(),
+          //   color: ThemeColors.grey1ThemeColor,
+          // ),
         ],
       ),
     );
