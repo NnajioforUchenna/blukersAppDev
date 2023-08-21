99184c7897f6637e16722e3248540079c45460e4
abdusman2014 <abdusman2014@gmail.com>
2023-08-22 01:48:02 +0500

    upgrader package integration

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,13 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
+import 'package:upgrader/upgrader.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Only call clearSavedSettings() during testing to reset internal values.
+ // await Upgrader.clearSavedSettings(); // REMOVE this for release builds
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -32,6 +36,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final appcastURL =
+    //     'https://raw.githubusercontent.com/larryaasen/upgrader/master/test/testappcast.xml';
+    // final cfg = AppcastConfiguration(url: appcastURL, supportedOS: ['android']);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => IndustriesProvider()),
@@ -48,56 +55,63 @@ class MyApp extends StatelessWidget {
             update: (_, user, WorkerProvider? previous) =>
                 previous!..update(user.appUser)),
       ],
-      child: ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return MaterialApp(
-            title: "Blukers",
-            debugShowCheckedModeBanner: false,
-            // LOCALIZATION
-            // - Manually set a locale:
-            // locale: const Locale('en'),
-            // - supportedLocales, if they match the phone's locale,
-            // flutter automatically use the langage for the given delegates.
-            // If they don't match the phone's locale, default locale will be 'en'.
-            supportedLocales: L10n.all,
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            //
-            builder: EasyLoading.init(),
-            theme: ThemeData(
-              // useMaterial3: true,
-              // colorScheme:
-              //     ColorScheme.fromSeed(seedColor: Color.fromRGBO(30, 117, 187, 1)),
-              // // Setup your ThemeData colorScheme manually:
-              // colorScheme: const ColorScheme(
-              //   brightness: Brightness.light,
-              //   primary: Color.fromRGBO(30, 117, 187, 1),
-              //   onPrimary: Colors.white,
-              //   secondary: Colors.white,
-              //   onSecondary: Color.fromARGB(255, 83, 83, 83),
-              //   error: Colors.red,
-              //   onError: Colors.white,
-              //   background: Colors.white,
-              //   onBackground: Colors.black,
-              //   surface: Colors.white,
-              //   onSurface: Color.fromRGBO(30, 117, 187, 1),
-              // ),
-              primarySwatch: Colors.blue,
-              fontFamily: 'Montserrat',
-            ),
-            // home: AuthenticationWrapper(),
-            onGenerateRoute: generateRoute,
-            initialRoute: "/",
-            navigatorKey: navigatorKey,
-          );
-        },
+      child: UpgradeAlert(
+          upgrader: Upgrader(
+        //  durationUntilAlertAgain: Duration(seconds: 2),
+         // debugDisplayAlways: true,
+          debugLogging: true,
+        ),
+        child: ScreenUtilInit(
+          designSize: const Size(360, 690),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp(
+              title: "Blukers",
+              debugShowCheckedModeBanner: false,
+              // LOCALIZATION
+              // - Manually set a locale:
+              // locale: const Locale('en'),
+              // - supportedLocales, if they match the phone's locale,
+              // flutter automatically use the langage for the given delegates.
+              // If they don't match the phone's locale, default locale will be 'en'.
+              supportedLocales: L10n.all,
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              //
+              builder: EasyLoading.init(),
+              theme: ThemeData(
+                // useMaterial3: true,
+                // colorScheme:
+                //     ColorScheme.fromSeed(seedColor: Color.fromRGBO(30, 117, 187, 1)),
+                // // Setup your ThemeData colorScheme manually:
+                // colorScheme: const ColorScheme(
+                //   brightness: Brightness.light,
+                //   primary: Color.fromRGBO(30, 117, 187, 1),
+                //   onPrimary: Colors.white,
+                //   secondary: Colors.white,
+                //   onSecondary: Color.fromARGB(255, 83, 83, 83),
+                //   error: Colors.red,
+                //   onError: Colors.white,
+                //   background: Colors.white,
+                //   onBackground: Colors.black,
+                //   surface: Colors.white,
+                //   onSurface: Color.fromRGBO(30, 117, 187, 1),
+                // ),
+                primarySwatch: Colors.blue,
+                fontFamily: 'Montserrat',
+              ),
+              // home: AuthenticationWrapper(),
+              onGenerateRoute: generateRoute,
+              initialRoute: "/",
+              navigatorKey: navigatorKey,
+            );
+          },
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_core
 import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -32,6 +33,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
