a67b94453c464062e11cf4b0e14338ea9a599d58
terra-labor <terralabor.dev@gmail.com>
2023-10-05 10:09:43 -0500

    form-crud

---
+++ lib/config/memberships/worker_membership.dart
@@ -0,0 +1,14 @@
+import 'memberships.dart';
+import 'actions/worker_actions.dart';
+
+class WorkerMembership {
+  static const String displayName = '';
+  static const String name = '';
+  static const String id = '';
+  static const String appleId = '';
+  static const String androidId = '';
+  static Map actions = {
+    [WorkerActions.applyToJobPosts]: true,
+    [WorkerActions.totalDailyJobPostsApplies]: 2
+  };
+}
