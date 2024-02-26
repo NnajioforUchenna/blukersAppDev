c683694256976d05b2270376405a728c6dda7dc8
100PercentFragranceTeam <151836753+100PercentFragranceTeam@users.noreply.github.com>
2024-02-25 20:23:07 -0600

    Begining work 250224

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,14 @@ import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    print('Error: $e');
+  }
+
   setPathUrlStrategy();
   runApp(MyApp());
 }
@@ -102,23 +107,6 @@ class MyApp extends StatelessWidget {
               //
               builder: EasyLoading.init(),
               theme: ThemeData(
-                // useMaterial3: true,
-                // colorScheme:
-                //     ColorScheme.fromSeed(seedColor: Color.fromRGBO(30, 117, 187, 1)),
-                // // Setup your ThemeData colorScheme manually:
-                // colorScheme: const ColorScheme(
-                //   brightness: Brightness.light,
-                //   primary: Color.fromRGBO(30, 117, 187, 1),
-                //   onPrimary: Colors.white,
-                //   secondary: Colors.white,
-                //   onSecondary: Color.fromARGB(255, 83, 83, 83),
-                //   error: Colors.red,
-                //   onError: Colors.white,
-                //   background: Colors.white,
-                //   onBackground: Colors.black,
-                //   surface: Colors.white,
-                //   onSurface: Color.fromRGBO(30, 117, 187, 1),
-                // ),
                 primarySwatch: Colors.blue,
                 fontFamily: 'Montserrat',
               ),
