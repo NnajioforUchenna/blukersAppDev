ed9a2b5a57a1a673a21ae6fb63f43dff58b3f44d
chris-eminence <peterabasiedimo@gmail,com>
2024-04-11 06:23:14 +0100

    Chat list

--- lib/views/worker/chat/chat_recipient_widget.dart
+++ lib/views/worker/chat/chat_recipient_widget.dart
@@ -2,11 +2,28 @@ import 'package:blukers/models/chat_recipient.dart';
 import 'package:flutter/material.dart';
 
 class ChatRecipientWidget extends StatelessWidget {
-  final ChatRecipient chatRecipient;
-  const ChatRecipientWidget({super.key, required this.chatRecipient});
+  final ChatRecipient? chatRecipient;
+  const ChatRecipientWidget({super.key, this.chatRecipient});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: GestureDetector(
+        onTap: (){
+          // Todo: The chat interface will be triggered here
+          // Todo: The recipient user ID will be collected
+        },
+        child: Material(
+          borderRadius: BorderRadius.circular(10),
+          elevation: 3,
+          child: ListTile(
+              leading: CircleAvatar(backgroundImage: AssetImage('assets/images/companyLogoPage.png')),
+            title: Text('Display name'),
+            subtitle: Text('Client type'),
+          ),
+        ),
+      ),
+    );
   }
 }
