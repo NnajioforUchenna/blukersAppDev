9bb3724eaadb3e245e41ae5da9228abc4fc87519
rillexhacks <ogukwee91@gmail.com>
2024-04-30 18:54:04 +0100

    second desktop translator

--- lib/views/old_common_views/select_industry_components/list_industries.dart
+++ lib/views/old_common_views/select_industry_components/list_industries.dart
@@ -1,3 +1,4 @@
+import 'package:blukers/views/worker/worker_home/worker_home_components/jobs_mobile_view/jobs_mobile_view_compnents/mobile_industry_headpanel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,11 +11,11 @@ class ListIndustries extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     IndustriesProvider ip = Provider.of<IndustriesProvider>(context);
-
     return Column(
       children: ip.industries.values.map((industry) {
-        return DisplayIndustry(
-          industry: industry,
+        return Container(
+          margin: const EdgeInsets.only(bottom: 10.0, left: 30, right: 30),
+          child: MobileIndustryHeadPanel(industry: industry),
         );
       }).toList(),
     );
--- lib/views/worker/worker_home/worker_home_components/jobs_desktop_view/jobs_page_desktop.dart
+++ lib/views/worker/worker_home/worker_home_components/jobs_desktop_view/jobs_page_desktop.dart
@@ -1,3 +1,10 @@
+
+import 'package:blukers/views/worker/worker_home/worker_home_components/jobs_desktop_view/select_desktop_language_dialog.dart';
+import 'package:blukers/views/worker/worker_home/worker_home_components/jobs_mobile_view/jobs_mobile_view_compnents/select_language_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:showcaseview/showcaseview.dart';
+
+import '../../../../../providers/app_settings_provider.dart';
 import '../../../../../providers/industry_provider.dart';
 import '../../../../../providers/job_posts_provider.dart';
 import 'package:flutter/material.dart';
@@ -9,14 +16,47 @@ import '../../../../old_common_views/select_industry_components/display_industri
 import '../../../saved/choose_target_language copy.dart';
 import '../../../saved/job_search_result_page.dart';
 
-class JobsPageDesktop extends StatelessWidget {
-  const JobsPageDesktop({super.key});
+class JobsPageDesktop extends StatefulWidget {
+  const JobsPageDesktop({Key? key}) : super(key: key);
 
+  @override
+  State<JobsPageDesktop> createState() => _JobsPageDesktopState();
+
+}
+class _JobsPageDesktopState extends State<JobsPageDesktop> {
+
+  @override
+    late AppSettingsProvider asp;
+
+    @override
+    void initState() {
+      super.initState();
+
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        final prefs = await SharedPreferences.getInstance();
+        final hasShowcased =
+            prefs.getBool('showcaseShown') ?? false; // Default to false
+
+        if (!hasShowcased) {
+          final asp = Provider.of<AppSettingsProvider>(context, listen: false);
+          final showcase = ShowCaseWidget.of(context);
+          showcase.startShowCase([
+            asp.signInButton,
+            asp.bottomNavigation,
+            asp.searchBar,
+            asp.selection,
+            asp.translation,
+          ]);
+          await prefs.setBool('showcaseShown', true);
+        }
+      });
+
+  }
   @override
   Widget build(BuildContext context) {
     IndustriesProvider ip = Provider.of<IndustriesProvider>(context);
     JobPostsProvider jp = Provider.of<JobPostsProvider>(context);
-
+    asp = Provider.of<AppSettingsProvider>(context);
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -25,7 +65,7 @@ class JobsPageDesktop extends StatelessWidget {
             height: 10,
           ),
           const Center(
-            child: ChooseTargetLanguage(),
+            child: SelectDesktopLanguageDialog(),
           ),
           const SizedBox(
             height: 10,
@@ -34,7 +74,17 @@ class JobsPageDesktop extends StatelessWidget {
           AnimatedCrossFade(
             firstChild: ip.industries.isEmpty
                 ? const LoadingPage()
-                : const DisplayIndustries(),
+                : Showcase(
+                key: asp.selection,
+                description: 'Use this section to Select Jobs by industry',
+                targetShapeBorder: const CircleBorder(),
+                overlayOpacity: 0.6,
+                tooltipBackgroundColor: Color.fromRGBO(30, 117, 187, 1),
+                descTextStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                child: const DisplayIndustries()),
             secondChild: const JobSearchResultPage(),
             crossFadeState: jp.isSearching
                 ? CrossFadeState.showSecond
