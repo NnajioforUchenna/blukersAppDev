5cfcc111f3128fa5a5128d34e281047bdad113a0
rillexhacks <ogukwee91@gmail.com>
2024-04-30 18:03:56 +0100

    translator for desktop

--- lib/views/old_common_views/select_industry_components/list_industries.dart
+++ lib/views/old_common_views/select_industry_components/list_industries.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../providers/industry_provider.dart';
+import '../../worker/worker_home/jobs_components/jobs_mobile_view/jobs_mobile_view_compnents/mobile_industry_headpanel.dart';
 import 'display_industry.dart';
 
 class ListIndustries extends StatelessWidget {
@@ -10,13 +11,14 @@ class ListIndustries extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     IndustriesProvider ip = Provider.of<IndustriesProvider>(context);
-
     return Column(
       children: ip.industries.values.map((industry) {
-        return DisplayIndustry(
-          industry: industry,
+        return Container(
+          margin: const EdgeInsets.only(bottom: 10.0, left: 30, right: 30),
+          child: MobileIndustryHeadPanel(industry: industry),
         );
       }).toList(),
     );
   }
 }
+
