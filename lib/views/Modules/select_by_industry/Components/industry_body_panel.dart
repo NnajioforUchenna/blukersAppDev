6cb6fb261882b8f946aafa7e8284b09854398968
Victor Marius <victormarius@Victors-MacBook-Pro.local>
2024-07-24 12:51:15 -0500

    implementing Menu Bar

---
+++ lib/views/Modules/select_by_industry/Components/industry_body_panel.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/job.dart';
+import '../../../utils/localization/localized_job_ids.dart';
+import '../../../utils/styles/theme_colors.dart';
+
+class IndustryBodyPanel extends StatelessWidget {
+  final List<Job> jobs;
+  final Function getRecords;
+  const IndustryBodyPanel(
+      {super.key, required this.jobs, required this.getRecords});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: jobs.length,
+      itemBuilder: (context, index) {
+        final job = jobs[index];
+        return InkWell(
+          onTap: () {
+            getRecords(job.jobId);
+          },
+          child: Container(
+              margin: const EdgeInsets.only(bottom: 5),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 7,
+                      child: Container(
+                        margin: const EdgeInsets.all(15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                text: LocalizedJobIds.get(context, job.title),
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: ThemeColors.primaryThemeColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                  Expanded(
+                      flex: 3,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 20),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.groups,
+                              color: ThemeColors.primaryThemeColor,
+                            ),
+                            Icon(Icons.arrow_forward_ios, color: Colors.grey)
+                          ],
+                        ),
+                      )),
+                ],
+              )),
+        );
+      },
+    );
+  }
+}
---
+++ lib/views/Modules/select_by_industry/Components/industry_head_panel.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../models/industry.dart';
+import '../../../services/on_hover.dart';
+import '../../../services/responsive.dart';
+import '../../../utils/localization/localized_industries.dart';
+import '../../../utils/styles/theme_colors.dart';
+import 'industry_body_panel.dart';
+
+class IndustryHeadPanel extends StatefulWidget {
+  final Industry industry;
+  final Function getRecords;
+  const IndustryHeadPanel(
+      {super.key, required this.industry, required this.getRecords});
+
+  @override
+  State<IndustryHeadPanel> createState() => _IndustryHeadPanelState();
+}
+
+class _IndustryHeadPanelState extends State<IndustryHeadPanel> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: OnHover(
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        width: Responsive.isDesktop(context) ? 70 : 50,
+                        height: Responsive.isDesktop(context) ? 70 : 50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit
+                                .cover, // This ensures the image covers the circular area
+                            image: AssetImage(widget.industry.imageUrl!),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: Responsive.isDesktop(context) ? 25 : 10),
+                    Expanded(
+                        child: Text(
+                      LocalizedIndustries.get(
+                          context, widget.industry.industryId),
+                      style: TextStyle(
+                        fontSize: Responsive.isDesktop(context) ? 30 : 20,
+                        fontWeight: FontWeight.w700,
+                        color: ThemeColors.grey1ThemeColor,
+                      ),
+                    )),
+                    Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        child: const Icon(
+                          Icons.arrow_forward_ios,
+                          color: ThemeColors.grey1ThemeColor,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        isExpanded
+            ? Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  AppLocalizations.of(context)!.selectAJobPosition,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: ThemeColors.secondaryThemeColor,
+                  ),
+                ),
+              )
+            : Container(),
+        isExpanded
+            ? IndustryBodyPanel(
+                jobs: widget.industry.jobs.values.toList(),
+                getRecords: widget.getRecords,
+              )
+            : Container(),
+      ],
+    );
+  }
+}
