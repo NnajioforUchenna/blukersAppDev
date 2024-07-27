3120f78038f3428214d7a4a28f8aefa5c64ff40b
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-07-27 15:33:35 +0100

    merging from main to anie

--- lib/views/company/worker_home/worker_home_components/workers_mobile_view/workers_mobile_view_components/worker_mobile_search_bar.dart
+++ lib/views/company/worker_home/worker_home_components/workers_mobile_view/workers_mobile_view_components/worker_mobile_search_bar.dart
@@ -29,71 +29,57 @@ class _WorkerMobileSearchBarState extends State<WorkerMobileSearchBar> {
     searchName =
         AppLocalizations.of(context)!.companySearchBarInput1Placeholder;
 
-    controller.text = "${jp.nameSearch} ${jp.locationSearch}";
+    if (jp.nameSearch.isNotEmpty || jp.locationSearch.isNotEmpty) {
+      controller.text = "${jp.nameSearch} ${jp.locationSearch}";
+    }
 
-    return Container(
-      height: screenHeight * 0.17,
-      decoration: const BoxDecoration(
-        color: ThemeColors.searchBarPrimaryThemeColor,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Center(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              margin: const EdgeInsets.only(bottom: 20),
-              child: TextField(
-                controller: controller,
-                onChanged: (value) {
-                  showDialog(
-                      context: context,
-                      builder: (context) => const SearchWorkersUi());
-                },
-                decoration: InputDecoration(
-                  isDense: true, // Added this
-                  contentPadding: const EdgeInsets.symmetric(
-                      vertical: 10, horizontal: 10), // Adjust padding
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: searchName,
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: IconButton(
-                      icon: const Icon(Icons.close),
-                      onPressed: controller.text.isEmpty
-                          ? null
-                          : () {
-                              controller.clear();
-                              jp.clearSearchParameters();
-                              GoRouter.of(context).pushReplacement('/workers');
-                            }),
-                  border: const OutlineInputBorder(),
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color: ThemeColors
-                            .blukersOrangeThemeColor), // Make sure to define ThemeColors.blukersOrangeThemeColor
-                  ),
-                ),
-                onTap: () {
-                  // Put your function here
-                  showDialog(
-                      context: context,
-                      builder: (context) => const SearchWorkersUi());
-                },
-                style: GoogleFonts.montserrat(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500), // Added this for smaller text
-              ),
-            ),
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.68,
+      height: 40,
+      child: TextField(
+        controller: controller,
+        onChanged: (value) {
+          showDialog(
+              context: context, builder: (context) => const SearchWorkersUi());
+        },
+        decoration: InputDecoration(
+          isDense: true,
+          // Added this
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          // Adjust padding
+          filled: true,
+          fillColor: Colors.white,
+          hintText: searchName,
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: controller.text.isEmpty
+                  ? null
+                  : () {
+                      controller.clear();
+                      jp.clearSearchParameters();
+                      GoRouter.of(context).pushReplacement('/workers');
+                    }),
+          border: const OutlineInputBorder(),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
           ),
-        ],
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+                color: ThemeColors
+                    .blukersOrangeThemeColor), // Make sure to define ThemeColors.blukersOrangeThemeColor
+          ),
+        ),
+        onTap: () {
+          // Put your function here
+          showDialog(
+              context: context, builder: (context) => const SearchWorkersUi());
+        },
+        style: GoogleFonts.montserrat(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: Colors.grey), // Added this for smaller text
       ),
     );
   }
