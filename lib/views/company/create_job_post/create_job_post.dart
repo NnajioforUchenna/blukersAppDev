518e92879aaf0b22c8207a02cdb1bc522e68ece3
chris-eminence <emichris380@gmail.com>
2024-08-01 11:00:26 +0100

    Web Create Job post inerface done

--- lib/views/company/create_job_post/create_job_post.dart
+++ lib/views/company/create_job_post/create_job_post.dart
@@ -1,6 +1,9 @@
+import 'package:blukers/views/company/create_job_post/creat_job_post_mobile.dart';
+import 'package:blukers/views/company/create_job_post/create_job_post_web.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../../services/responsive.dart';
 import 'create_job_post_components/job_post_page_slider.dart';
 import 'create_job_post_components/job_post_time_line.dart';
 
@@ -9,28 +12,8 @@ class CreateJobPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.createJobPost),
-      ),
-      body: Column(
-        children: [
-          JobPostTimeline(),
-          Expanded(
-            child: CustomScrollView(
-              slivers: [
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      const JobPostPageSlider(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+    return Responsive.isMobile(context)
+        ? const CreateJobPostMobile()
+        : const CreateJobPostWeb();
   }
 }
