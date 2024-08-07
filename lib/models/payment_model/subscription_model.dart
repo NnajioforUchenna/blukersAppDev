c0fb54308b09207a0da42470f13cc83f1888b17e
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-08-07 14:24:19 +0100

    mergin main

--- lib/models/payment_model/subscription_model.dart
+++ lib/models/payment_model/subscription_model.dart
@@ -15,7 +15,7 @@ class SubscriptionPlan {
     required this.paymentPlatform,
   });
 
-  // Convert ActiveSubscription object into a map
+  // Convert SubscriptionPlan object into a map
   Map<String, dynamic> toMap() {
     return {
       'subscribeDate': subscribeDate,
@@ -27,20 +27,34 @@ class SubscriptionPlan {
     };
   }
 
-  // Create ActiveSubscription object from a map
+  // Create SubscriptionPlan object from a map
   static SubscriptionPlan fromMap(Map<String, dynamic> map) {
-    return SubscriptionPlan(
-      subscribeDate: map['subscribeDate'],
-      renewDate: map['renewDate'],
-      subscriptionId: map['subscriptionId'],
-      subscriptionName: map['subscriptionName'],
-      amountPaid: map['amountPaid'],
-      paymentPlatform: map['paymentPlatform'],
-    );
+    try {
+      return SubscriptionPlan(
+        subscribeDate: map['subscribeDate'],
+        renewDate: map['renewDate'],
+        subscriptionId: map['subscriptionId'],
+        subscriptionName: map['subscriptionName'],
+        amountPaid: map['amountPaid'],
+        paymentPlatform: map['paymentPlatform'],
+      );
+    } catch (e) {
+      // Log the error or handle it as necessary
+      print('Error converting map to SubscriptionPlan: $e');
+      // Optionally, return a default object or null
+      return SubscriptionPlan(
+        subscribeDate: 0,
+        renewDate: 0,
+        subscriptionId: '',
+        subscriptionName: '',
+        amountPaid: 0.0,
+        paymentPlatform: '',
+      );
+    }
   }
 
   @override
   String toString() {
-    return 'ActiveSubscription(subscribeDate: $subscribeDate, renewDate: $renewDate, subscriptionId: $subscriptionId, subscriptionName: $subscriptionName, amountPaid: $amountPaid, paymentPlatform: $paymentPlatform)';
+    return 'SubscriptionPlan(subscribeDate: $subscribeDate, renewDate: $renewDate, subscriptionId: $subscriptionId, subscriptionName: $subscriptionName, amountPaid: $amountPaid, paymentPlatform: $paymentPlatform)';
   }
 }
