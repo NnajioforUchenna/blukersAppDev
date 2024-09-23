a143166e06781f9d5709c4917eba8a1ebd0cc52b
Aladeen <aladeenuthy@gmail.com>
2024-09-23 12:02:26 +0100

    fix to saved job

--- lib/views/worker/saved/time_ago_and_bookmark_row.dart
+++ lib/views/worker/saved/time_ago_and_bookmark_row.dart
@@ -35,11 +35,11 @@ class _TimeAgoAndBookMarkRowState extends State<TimeAgoAndBookMarkRow> {
                   //     builder: (context) => const DisplayJobTimelineDialog(),
                   //   );
                   // } else {
-                    // Save or unsave the job post without showing a loading spinner
-                    await up.saveJobPost(widget.jobPost);
+                  // Save or unsave the job post without showing a loading spinner
+                  await up.saveJobPost(widget.jobPost);
 
-                    // Trigger rebuild to reflect the change
-                    setState(() {});
+                  // Trigger rebuild to reflect the change
+                  setState(() {});
                   // }
                 },
                 icon: Icon(
@@ -82,22 +82,20 @@ class _SavedJobsIconState extends State<SavedJobsIcon> {
                   builder: (context) => const DisplayJobTimelineDialog(),
                 );
               } else {
-                // Show loading spinner
-                showDialog(
-                  context: context,
-                  barrierDismissible: false, // Prevent dismissing the dialog
-                  builder: (context) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                );
+                // // Show loading spinner
+                // showDialog(
+                //   context: context,
+                //   barrierDismissible: false, // Prevent dismissing the dialog
+                //   builder: (context) {
+                //     return const Center(
+                //       child: CircularProgressIndicator(),
+                //     );
+                //   },
+                // );
 
                 // Save or unsave the job post and wait until the action is completed
                 await up.saveJobPost(widget.jobPost);
-
-                // Close the loading spinner
-                Navigator.of(context).pop();
+               
 
                 // Trigger rebuild to reflect the change
                 setState(() {});
