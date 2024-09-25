c5c6652a03e9068a053c44ca8127a8a55ee45298
Aladeen <aladeenuthy@gmail.com>
2024-09-25 06:10:21 +0100

    company profile redesign

--- lib/views/company/company_profile/components/update_company_basic_information.dart
+++ lib/views/company/company_profile/components/update_company_basic_information.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../../providers/user_provider_parts/user_provider.dart';
 import '../../../../../utils/styles/index.dart';
 import '../../../../services/responsive.dart';
-import '../../../old_common_views/small_pop_button_widget.dart';
+import 'info_input_widget.dart';
 
 class UpdateCompanyBasicInformation extends StatefulWidget {
   const UpdateCompanyBasicInformation({super.key});
@@ -42,138 +41,110 @@ class _UpdateCompanyBasicInformationState
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Dialog(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(5)),
+    return Container(
+      // width: double.infinity,
+      padding: EdgeInsets.only(
+        top: 56,
+        bottom: 56,
+        left: Responsive.isMobile(context) ? 25 : 40,
+        right: Responsive.isMobile(context) ? 25 : 40,
       ),
-      insetPadding:
-          const EdgeInsets.only(left: 16, right: 16, top: 26, bottom: 80),
-      child: Stack(
-        alignment: Alignment.topCenter,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            margin: EdgeInsets.only(top: height * 0.05, bottom: height * 0.05),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                        top: height * 0.05, bottom: height * 0.06),
-                    child: Text(
-                      'Basic Information',
-                      style: GoogleFonts.montserrat(
-                          fontWeight: FontWeight.bold, fontSize: 24),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // const Row(
+            //   mainAxisAlignment: MainAxisAlignment.end,
+            //   children: [SmallPopButtonWidget()],
+            // ),
+            Row(
+              mainAxisAlignment: Responsive.isMobile(context)
+                  ? MainAxisAlignment.center
+                  : MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Basic Information',
+                  style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Responsive.isMobile(context) ? 20 : 29),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 56,
+            ),
+            Column(
+              children: [
+                InfoInputWidget(
+                  label: 'Company Name',
+                  controller: nameController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InfoInputWidget(
+                  label: 'Email',
+                  controller: emailController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InfoInputWidget(
+                  textInputType: TextInputType.number,
+                  label: 'Phone Number',
+                  controller: phoneNumberController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                InfoInputWidget(
+                  label: 'Description',
+                  maxLines: 4,
+                  controller: descriptionController,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            Center(
+              child: SizedBox(
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    UserProvider up =
+                        Provider.of<UserProvider>(context, listen: false);
+                    up.updateCompanyBasicInformation(
+                      name: nameController.text,
+                      description: descriptionController.text,
+                      email: [emailController.text],
+                      phoneNumber: [phoneNumberController.text],
+                    );
+                    if (Responsive.isMobile(context)) {
+                      Navigator.pop(context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: ThemeColors.secondaryThemeColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: width * 0.1),
-                    child: Column(
-                      children: [
-                        TextInputWigdet(
-                          label: 'Company Name',
-                          maxlines: 1,
-                          controller: nameController,
-                        ),
-                        TextInputWigdet(
-                          label: 'Email',
-                          maxlines: 1,
-                          controller: emailController,
-                        ),
-                        TextInputWigdet(
-                          label: 'Phone Number',
-                          maxlines: 1,
-                          controller: phoneNumberController,
-                        ),
-                        const SizedBox(height: 10),
-                        TextInputWigdet(
-                          label: 'Description',
-                          maxlines: 5,
-                          controller: descriptionController,
-                        ),
-                      ],
+                  child: Text(
+                    'Update Changes',
+                    style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontSize: Responsive.isMobile(context) ? 16 : 18,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  Container(
-                    height: height * 0.03,
-                    width: width * 0.30,
-                    margin: EdgeInsets.only(top: height * 0.03, bottom: 30.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        UserProvider up =
-                            Provider.of<UserProvider>(context, listen: false);
-                        up.updateCompanyBasicInformation(
-                          name: nameController.text,
-                          description: descriptionController.text,
-                          email: [emailController.text],
-                          phoneNumber: [phoneNumberController.text],
-                        );
-                        Navigator.pop(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        elevation: 5,
-                        backgroundColor: ThemeColors.secondaryThemeColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                      ),
-                      child: Text(
-                        'Update',
-                        style: GoogleFonts.montserrat(
-                          color: Colors.white,
-                          fontSize: Responsive.isMobile(context) ? 9.sp : 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-          const Positioned(
-            top: 10, // Adjust as needed
-            left: 10, // Adjust as needed
-            child: SmallPopButtonWidget(),
-          ),
-        ],
-      ),
-    );
-  }
-
-  TextInputWigdet(
-      {required String label,
-      required int maxlines,
-      required TextEditingController controller}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: GoogleFonts.montserrat(
-            fontWeight: FontWeight.w400,
-            fontSize: 12,
-          ),
+          ],
         ),
-        const SizedBox(height: 5),
-        TextFormField(
-          controller: controller,
-          maxLines: maxlines,
-          decoration: InputDecoration(
-            filled: true,
-            isDense: true,
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
-            fillColor: Colors.grey[200],
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              borderSide: BorderSide.none,
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
--- lib/views/old_common_views/components/confirmation_dialog.dart
+++ lib/views/old_common_views/components/confirmation_dialog.dart
@@ -1,5 +1,7 @@
+import 'package:blukers/services/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../utils/styles/index.dart';
 
@@ -16,7 +18,7 @@ void confirmationDialog({
     title = AppLocalizations.of(context)!.logout;
     text = AppLocalizations.of(context)!.areYouSureYouWantToLogOut;
     cancelText = AppLocalizations.of(context)!.cancel;
-    confirmText = AppLocalizations.of(context)!.yes;
+    confirmText = AppLocalizations.of(context)!.logout;
   }
 
   if (stringsTemplate == "deleteAccount") {
@@ -30,21 +32,32 @@ void confirmationDialog({
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
+        backgroundColor: Colors.white,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         actionsAlignment: MainAxisAlignment.center,
-        title: Text(
-          title,
-          style: const TextStyle(
-            color: ThemeColors.blukersBlueThemeColor,
-            fontWeight: FontWeight.bold,
+        title: Center(
+          child: Text(
+            title,
+            style: GoogleFonts.montserrat(
+              fontSize: Responsive.isMobile(context) ? 16 : 28,
+              color: stringsTemplate == "deleteAccount"
+                  ? const Color(0xFFBC0101)
+                  : ThemeColors.blukersBlueThemeColor,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
-        content: Text(
-          text,
-          style: const TextStyle(color: ThemeColors.grey1ThemeColor),
-        ),
+        content: Text(text,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.montserrat(
+              fontSize: Responsive.isMobile(context) ? 10 : 16,
+              color: stringsTemplate == "deleteAccount"
+                  ? const Color(0xFFC3AEAE)
+                  : ThemeColors.grey1ThemeColor,
+              fontWeight: FontWeight.w500,
+            )),
         actions: <Widget>[
           Column(
             children: [
@@ -54,20 +67,31 @@ void confirmationDialog({
                   Navigator.of(context).pop();
                 },
                 style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(300, 50),
-                  backgroundColor: const Color.fromARGB(255, 205, 205, 205),
+                  backgroundColor: Colors.white,
+                  minimumSize: Size(Responsive.isMobile(context) ? 110 : 180,
+                      Responsive.isMobile(context) ? 42 : 50),
                   padding:
                       const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                  textStyle: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Montserrat',
-                  ),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: stringsTemplate == "deleteAccount"
+                          ? const Color(0xFFBC0101)
+                          : ThemeColors.blukersOrangeThemeColor,
+                      width: 1,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(8.0)),
                   ),
                 ),
-                child: Text(confirmText.toUpperCase()),
+                child: Text(
+                  confirmText,
+                  style: GoogleFonts.montserrat(
+                    fontSize: Responsive.isMobile(context) ? 12 : 18,
+                    fontWeight: FontWeight.w600,
+                    color: stringsTemplate == "deleteAccount"
+                        ? const Color(0xFFBC0101)
+                        : ThemeColors.blukersOrangeThemeColor,
+                  ),
+                ),
               ),
               const SizedBox(height: 10),
               ElevatedButton(
@@ -75,20 +99,22 @@ void confirmationDialog({
                   Navigator.of(context).pop();
                 },
                 style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(300, 50),
+                  minimumSize: Size(Responsive.isMobile(context) ? 110 : 180,
+                      Responsive.isMobile(context) ? 42 : 50),
                   backgroundColor: ThemeColors.blukersOrangeThemeColor,
                   padding:
                       const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                  textStyle: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Montserrat',
-                  ),
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8.0)),
                   ),
                 ),
-                child: Text(cancelText.toUpperCase()),
+                child: Text(
+                  cancelText,
+                  style: GoogleFonts.montserrat(
+                      fontSize: Responsive.isMobile(context) ? 12 : 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
               ),
               const SizedBox(height: 20),
             ],
